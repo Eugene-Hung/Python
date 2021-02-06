ad18e141d4bdfbde28c5d74fd56e2475f5c74a55
Eugene Hung <eugene-hung@hotmail.com>
2021-02-06 18:42:25 +0800

    chang value name

--- qcc_config_tool_1.py
+++ qcc_config_tool_1.py
@@ -21,27 +21,27 @@ class Eugene_GUI():
     def __init__(self, init_window_name):
         self.init_window_name = init_window_name
 
-        self.status_value      = IntVar()
-        self.latest_ver        = StringVar()
-        self.current_ver       = StringVar()
-        self.firmware_ver      = StringVar()
-        self.bluetooth_name    = StringVar() 
-        self.bluetooth_address = StringVar()
-        self.final_result  = StringVar()
-        self.count         = StringVar()
-        self.count_success = StringVar()
-        self.count_faied   = StringVar()
+        self.state_val   = IntVar()
+        self.latest_ver  = StringVar()
+        self.current_ver = StringVar()
+        self.fw_vers  = StringVar()
+        self.bt_name  = StringVar() 
+        self.bt_addr  = StringVar()
+        self.fin_resl = StringVar()
+        self.all_val  = StringVar()
+        self.succ_val = StringVar()
+        self.fail_val = StringVar()
 
-        self.status_value.  set("0")
-        self.latest_ver.    set("")
-        self.current_ver.   set("")
-        self.firmware_ver.  set("")
-        self.bluetooth_name.set("")
-        self.bluetooth_address.set("")
-        self.final_result. set("")
-        self.count.        set("0")
-        self.count_success.set("0")
-        self.count_faied.  set("0")
+        self.state_val.  set("0")
+        self.latest_ver. set("")
+        self.current_ver.set("")
+        self.fw_vers.set("")
+        self.bt_name.set("")
+        self.bt_addr.set("")
+        self.fin_resl.set("")
+        self.all_val. set("0")
+        self.succ_val.set("0")
+        self.fail_val.set("0")
 
     def cmd(self, command):
         global process_status
@@ -53,13 +53,6 @@ class Eugene_GUI():
         else:
             process_status = False
 
-    def QccReadConfigCmd(self):
-        global process_status
-        global count_dut
-        write_list = []
-        self.final_result.set("")
-        read_qcc_command = r".\ConfigCmd.exe dev2txt qcc_configure.txt -usbdbg 1 -database hyd.sdb -system QCC512X_CONFIG"
-        self.cmd(read_qcc_command)
 
     def WindowConfigure(self, w, h):
         self.init_window_name.title(" Qualcomm Verson Check Tool V1.0.0")
@@ -80,7 +73,7 @@ class Eugene_GUI():
         self.fw_ver_label  = Label(self.init_window_name, text="  固件版本    ", width = 15,height = 2, fg = "teal", font=("微软雅黑", "10", "bold"))
         self.bt_name_label = Label(self.init_window_name, text="  蓝牙名称    ", width = 15,height = 2, font=("微软雅黑", "10", "bold"))
         self.bt_addr_label = Label(self.init_window_name, text="  蓝牙地址    ", width = 15,height = 2, font=("微软雅黑", "10", "bold"))
-        self.fin_res_label = Label(self.init_window_name, text="  对比结果    ", width = 15,height = 2, font=("微软雅黑", "10", "bold"))
+        self.fin_resl_label = Label(self.init_window_name, text="  对比结果    ", width = 15,height = 2, font=("微软雅黑", "10", "bold"))
         self.count_a_label = Label(self.init_window_name, text="  当前检测总数 ", width = 15,height = 2, font=("微软雅黑", "10", "bold"))
         self.count_s_label = Label(self.init_window_name, text="  校对成功总数 ", width = 15,height = 2, font=("微软雅黑", "10", "bold"))
         self.count_f_label = Label(self.init_window_name, text="  校对失败总数 ", width = 15,height = 2, font=("微软雅黑", "10", "bold"))
@@ -90,7 +83,7 @@ class Eugene_GUI():
         self.fw_ver_label.grid (row = label_x + 2, column = label_y + 0)
         self.bt_name_label.grid(row = label_x + 3, column = label_y + 0)
         self.bt_addr_label.grid(row = label_x + 4, column = label_y + 0)
-        self.fin_res_label.grid(row = label_x + 5, column = label_y + 0)
+        self.fin_resl_label.grid(row = label_x + 5, column = label_y + 0)
         self.count_a_label.grid(row = label_x + 6, column = label_y + 0)
         self.count_s_label.grid(row = label_x + 6, column = label_y + 1)
         self.count_f_label.grid(row = label_x + 6, column = label_y + 2)
@@ -102,20 +95,20 @@ class Eugene_GUI():
 
         self.lat_ver_entry  = Entry(self.init_window_name, textvariable = self.latest_ver,        width = 25)
         self.cur_ver_entry  = Entry(self.init_window_name, textvariable = self.current_ver,       width = 25)
-        self.fw_ver_entry   = Entry(self.init_window_name, textvariable = self.firmware_ver,      width = 25)
-        self.bt_name_entry  = Entry(self.init_window_name, textvariable = self.bluetooth_name,    width = 25)
-        self.bt_addr_entry  = Entry(self.init_window_name, textvariable = self.bluetooth_address, width = 25)
-        self.fin_res_entry  = Entry(self.init_window_name, textvariable = self.final_result, width=25, foreground = "red", state = "readonly")
-        self.count_a_entry  = Entry(self.init_window_name, textvariable = self.count, width=6, font=("微软雅黑", "12", "bold"),state = "readonly")
-        self.count_s_entry  = Entry(self.init_window_name, textvariable = self.count_success, width=6, font=("微软雅黑", "12", "bold"),state = "readonly", foreground = "blue")
-        self.count_f_entry  = Entry(self.init_window_name, textvariable = self.count_faied, width=6, font=("微软雅黑", "12", "bold"),state = "readonly", foreground = "red")
+        self.fw_ver_entry   = Entry(self.init_window_name, textvariable = self.fw_vers,      width = 25)
+        self.bt_name_entry  = Entry(self.init_window_name, textvariable = self.bt_name,    width = 25)
+        self.bt_addr_entry  = Entry(self.init_window_name, textvariable = self.bt_addr, width = 25)
+        self.fin_resl_entry  = Entry(self.init_window_name, textvariable = self.fin_resl, width=25, foreground = "red", state = "readonly")
+        self.count_a_entry  = Entry(self.init_window_name, textvariable = self.all_val, width=6, font=("微软雅黑", "12", "bold"),state = "readonly")
+        self.count_s_entry  = Entry(self.init_window_name, textvariable = self.succ_val, width=6, font=("微软雅黑", "12", "bold"),state = "readonly", foreground = "blue")
+        self.count_f_entry  = Entry(self.init_window_name, textvariable = self.fail_val, width=6, font=("微软雅黑", "12", "bold"),state = "readonly", foreground = "red")
         
         self.lat_ver_entry.grid(row = entry_x + 0, column = entry_y + 1)
         self.cur_ver_entry.grid(row = entry_x + 1, column = entry_y + 1)
         self.fw_ver_entry.grid (row = entry_x + 2, column = entry_y + 1)
         self.bt_name_entry.grid(row = entry_x + 3, column = entry_y + 1)
         self.bt_addr_entry.grid(row = entry_x + 4, column = entry_y + 1)
-        self.fin_res_entry.grid(row = entry_x + 5, column = entry_y + 1)
+        self.fin_resl_entry.grid(row = entry_x + 5, column = entry_y + 1)
         self.count_a_entry.grid(row = entry_x + 7, column = entry_y + 0)
         self.count_s_entry.grid(row = entry_x + 7, column = entry_y + 1)
         self.count_f_entry.grid(row = entry_x + 7, column = entry_y + 2)
@@ -123,20 +116,84 @@ class Eugene_GUI():
         button_x = 1
         button_y = 1
 
-        self.lock_button = Checkbutton(self.init_window_name, text = '锁定版本号',variable = self.status_value,command = self.Status_Entry)
-        self.read_buttom = Button(self.init_window_name, text=" 进行读取 ", bg="lightblue", width=10)
-
+        self.lock_button = Checkbutton(self.init_window_name, text = '锁定版本号',variable = self.state_val,command = self.Status_Entry)
+        self.read_button = Button(self.init_window_name, text=" 进行读取 ", bg="lightblue"  , width=10, command = self.QccReadConfigCmd)
+        self.comp_button = Button(self.init_window_name, text=" 开始对比 ", bg="lightyellow", width=10, command = self.StartComparing)
         
-        self.lock_button.grid(row = button_x + 0, column = button_y+2)
-        self.read_buttom.grid(row = button_x + 1, column = button_y+2)
+        self.lock_button.grid(row = button_x + 0, column = button_y + 2)
+        self.read_button.grid(row = button_x + 1, column = button_y + 2)
+        self.comp_button.grid(row = button_x + 2, column = button_y + 2)
     
     def Status_Entry(self):
-        if self.status_value.get() == True :
+        if self.state_val.get() == True :
             Entry(self.init_window_name , textvariable=self.latest_ver, width=20, state="readonly"  ).grid(row=1, column=2)
         else :
             Entry(self.init_window_name , textvariable=self.latest_ver, width=20, state="normal"  ).grid(row=1, column=2)
 
+    def ClearInputValue(self):
+        self.current_ver.set("")
+        self.fin_resl.set("")
+        self.fw_vers.set("")
+        self.bt_name.set("")
+        self.bt_addr.set("")
+    
+    def QccReadConfigCmd(self):
+        global process_status
+        global count_dut
 
+        write_list = []
+        self.fin_resl.set("")
+        read_qcc_command = r".\ConfigCmd.exe dev2txt qcc_configure.txt -usbdbg 1 -database hyd.sdb -system QCC512X_CONFIG"
+        self.cmd(read_qcc_command)
+
+        if process_status == False:
+            self.ClearInputValue()
+            showerror(title='Read failure', message='找不到待测产品')
+            return
+        else:
+            count_dut += 1
+            self.all_val.set(count_dut)
+            eugene_file = open(r"./qcc_configure.txt", "r")
+            for index in range(25):
+                write_list = eugene_file.readline()
+                if index == 18:
+                    blue_address_list = write_list[56:60] + "-" + write_list[39:41] + "-" + write_list[18:25]
+                    self.bt_addr.set(blue_address_list)
+                if index == 21:
+                    self.bt_name.set(write_list[21:38])
+            write_str   = write_list[20:48:3]
+            version_str = write_list[44:48:3]
+            version_str = "V = " + version_str
+            self.current_ver .set(write_str)
+            self.fw_vers.set(version_str)
+            eugene_file.close()
+            os.remove(r"./qcc_configure.txt")
+    
+    def ConfirmLatestVerson(self):
+        InputVersonValue = self.latest_ver.get()
+        self.latest_ver.set(InputVersonValue)
+
+        return InputVersonValue
+
+    def StartComparing(self):
+        global test_success
+        global test_failed
+        extract_list = self.ConfirmLatestVerson()
+        if not extract_list:
+            showinfo(title='Tips by Eugene', message='请先输入目标版本号')
+        else:
+            read_list = self.current_ver.get()
+            if not read_list:
+                showinfo(title='Tips by Eugene', message='请先进行读取DUT信息')
+            else :
+              compare_reslut = op.eq(read_list, extract_list)
+              if compare_reslut is True:
+                self.fin_resl.set("Success")
+                test_success += 1
+              else:
+                self.fin_resl.set("Failed")
+                showwarning(title='Check failure', message='当前版本号与输入的版本号不一致')
+                test_failed += 1
 
 
 def EugeneGUIStart():
